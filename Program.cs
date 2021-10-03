f2b36055949e857b9d63b1615fd801a701113204
matheus-nbx52 <matheusfssilva2001@gmail.com>
2021-10-03 19:30:45 -0300

    atualização

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace rpg
     {
         static void Main(string[] args)
         {   //Como relacionar objeto com letra na posição do grid?
-            //
+            //instancia de objeto
             Hero frodo = new Hero();
             Monster Orc  = new Monster();
 
